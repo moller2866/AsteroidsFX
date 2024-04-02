042a398768a1601a3381302c74b45a7b968ab743
Moller2866 <kaspermoeller.t@gmail.com>
2024-04-02 20:21:19 +0200

    added java-spring beans

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -26,14 +26,11 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main extends Application {
 
-    private final GameData gameData = new GameData();
-    private final World world = new World();
-    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
-
-    private Pane gameWindow;
+    private AnnotationConfigApplicationContext context;
 
     public static void main(String[] args) {
         launch(Main.class);
@@ -41,122 +38,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception {
-        Text text = new Text(10, 20, "Destroyed asteroids: 0");
-        gameWindow = new Pane();
-        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
-        gameWindow.getChildren().add(text);
-
-        Scene scene = new Scene(gameWindow);
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode().equals(KeyCode.LEFT)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, true);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, true);
-            }
-            if (event.getCode().equals(KeyCode.UP)) {
-                gameData.getKeys().setKey(GameKeys.UP, true);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, true);
-            }
-        });
-        scene.setOnKeyReleased(event -> {
-            if (event.getCode().equals(KeyCode.LEFT)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, false);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, false);
-            }
-            if (event.getCode().equals(KeyCode.UP)) {
-                gameData.getKeys().setKey(GameKeys.UP, false);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, false);
-            }
-
-        });
-
-        // Lookup all Game Plugins using ServiceLoader
-        for (IGamePluginService iGamePlugin : getPluginServices()) {
-            iGamePlugin.start(gameData, world);
+        context = new AnnotationConfigApplicationContext(ModuleConfig.class);
+        for (String beanDefinitionName : context.getBeanDefinitionNames()) {
+            System.out.println(beanDefinitionName);
         }
-        for (Entity entity : world.getEntities()) {
-            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
-            polygons.put(entity, polygon);
-            gameWindow.getChildren().add(polygon);
-        }
-
-        render();
-
-        window.setScene(scene);
-        window.setTitle("ASTEROIDS");
-        window.show();
-
+        Game game = context.getBean(Game.class);
+        game.start(window);
+        game.render();
     }
 
-    private void render() {
-        new AnimationTimer() {
-            private long then = 0;
-
-            @Override
-            public void handle(long now) {
-                update();
-                draw();
-                gameData.getKeys().update();
-            }
-
-        }.start();
-    }
-
-    private void update() {
-
-        // Update
-        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
-            entityProcessorService.process(gameData, world);
-        }
-        for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
-            postEntityProcessorService.process(gameData, world);
-        }
-    }
     @Override
     public void stop() {
-        for (IGamePluginService iGamePlugin : getPluginServices()) {
-            iGamePlugin.stop(gameData, world);
-        }
-    }
-
-    private void draw() {
-
-        polygons.forEach((key, value) -> {
-            if (!world.getEntities().contains(key)) {
-                gameWindow.getChildren().remove(value);
-                polygons.remove(key);
-            }
-        });
-
-        for (Entity entity : world.getEntities()) {
-            Polygon polygon = polygons.get(entity);
-            if (polygon == null) {
-                polygon = new Polygon(entity.getPolygonCoordinates());
-                polygons.put(entity, polygon);
-                gameWindow.getChildren().add(polygon);
-            }
-            polygon.setTranslateX(entity.getX());
-            polygon.setTranslateY(entity.getY());
-            polygon.setRotate(entity.getRotation());
-        }
-    }
-
-    private Collection<? extends IGamePluginService> getPluginServices() {
-        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
-        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
-        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+        Game game = context.getBean(Game.class);
+        game.stop();
     }
 }
